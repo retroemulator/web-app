9931ad22cbce1c8b726895c4f47b35ce648b4b4a
Eric Liu <ericliu314@gmail.com>
2019-12-20 18:16:37 -0500

    Update

--- src/server/src/api/v1/router.js
+++ src/server/src/api/v1/router.js
@@ -1,7 +1,8 @@
-// TODO: document REST API with Swagger
+const axios = require('axios');
 const express = require('express');
-const firestore = require('../../firestore');
+const Consoles = require('@retroemulator/core/src/enums/consoles');
 const { generateError, Errors } = require('@retroemulator/core/src/errors');
+const firestore = require('../../firestore');
 
 const router = express.Router();
 
@@ -11,7 +12,7 @@ const router = express.Router();
  * Gets listing of all available consoles.
  */
 router.get('/console', async (req, res) => {
-  res.setHeader('ContentType', 'application/json');
+  res.setHeader('Content-Type', 'application/json');
   const doc = await firestore.collection('resources').doc('v1').get();
   if (!doc.exists) {
     return res.status(400).send(generateError(
@@ -19,8 +20,41 @@ router.get('/console', async (req, res) => {
       Errors.WebApp.CONSOLES_LISTING_NOT_EXIST.message,
     ));
   }
+  return res.status(200).send({ data: doc.data() });
+});
 
-  res.status(200).send({ data: doc.data() });
+/**
+ * POST /create-session
+ *
+ * @param consoleId
+ *
+ * Makes a request to SessionService to create a new session.
+ */
+router.post('/create-session', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  const { consoleId } = req.body;
+  if (consoleId === undefined || !Consoles.get(consoleId)) {
+    return res.status(400).send(generateError(
+      Errors.Generic.INVALID_CONSOLE_ID.code,
+      Errors.Generic.INVALID_CONSOLE_ID.message,
+    ));
+  }
+
+  const url = `${process.env.SESSION_SERVICE_URL}/create-session`;
+
+  try {
+    const result = await axios.post(url, req.body);
+    console.log(result.data); // remove this later
+    return res.status(200).send({ data: result.data });
+  } catch (error) {
+    return res.status(400).send(error.response.data);
+  }
+});
+
+router.post('/join-session', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  return res.status(200).send();
 });
 
 module.exports = router;
